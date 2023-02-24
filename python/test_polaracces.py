31c53322b9cf62176305c918e39e7b483e9facaf
MarcRotsaert <mglrot@caiway.nl>
2023-02-24 21:34:38 +0100

    polar accesslink  start

---
+++ python/test_polaracces.py
@@ -0,0 +1,15 @@
+import requests
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Authorization": "Basic dGhpc2RvZXNudDpkb2FueXRoaW6s",
+    "Accept": "application/json",
+}
+body = {
+    "grant_type": "authorization_code",
+    "authorization_code": "SplxlOBeZQQYbYS6WxSbIA",
+}
+
+r = requests.post("https://polarremote.com/v2/oauth2/token", data=body, headers=headers)
+
+print(r.json())
