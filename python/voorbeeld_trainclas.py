866513fd5a6c4c7a00bf93153c6e6b2c50a9f9bf
MarcRotsaert <mglrot@caiway.nl>
2023-11-02 21:41:12 +0100

    training description to database

--- python/voorbeeld_trainclas.py
+++ python/voorbeeld_trainclas.py
@@ -1,8 +1,7 @@
-
 from training_classifier import MongoRunningClassifier
 
-
 classif = MongoRunningClassifier("polartest4", "polar2017")
+
 classif.set_easyrun()
 easyrun, no_easyrun = classif.return_easyrun()
 road_races = classif.mongo.simplequery("trainingtype.easyrun", True)
@@ -38,3 +37,4 @@ for rr in road_races:
     print(rr["fname"])
 print("Finish___________________________________________________")
 
+classif.set_traindescription()
