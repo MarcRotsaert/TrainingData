a01d6ab4096202a28897d64b76bddcc052487c41
MarcRotsaert <mglrot@caiway.nl>
2023-11-09 22:49:57 +0100

    Very simple html with javascript

--- js/read_toml2.js
+++ js/read_toml2.js
@@ -1,38 +1,62 @@
-var toml = require("toml")
-var concat = require('concat-stream');
-var fs = require("fs")
+// var toml = require("toml")
+// var concat = require('concat-stream');
+// var fs = require("fs")
 
-// Specify the path to your TOML file
-const tomlFilePath = "config_dummy.toml";
 
-// Create a read stream for the TOML file
-const readStream = fs.createReadStream(tomlFilePath, 'utf8');
 
-let data = '';
+// script.js
 
-readStream.on('data', (chunk) => {
-    data += chunk;
-});
+// Define a function that updates the content of the "demo" paragraph
+function updateDemo() {
+    var demoElement = document.getElementById("button");
+    demoElement.innerHTML = "Hello, World! (from a function)";
+}
 
-readStream.on('end', () => {
-    // Now, the `data` variable contains the entire contents of the TOML file as a string
-    console.log(data);
+function selectText2() {
+    var testElement = document.getElementById("trainingtype")
+    console.log(testElement.value)
+}
 
-    // You can parse it as TOML if needed
-    const toml = require('toml');
 
-    try {
-        parsedData = toml.parse(data);
-    } catch (e) {
-        console.error("Parsing error on line " + e.line + ", column " + e.column +
-            ": " + e.message);
-    }
+// Add an event listener to call the function when the button is clicked
+document.getElementById("button").addEventListener("click", selectText2);
 
-    // Now you can work with the parsed TOML data
-    console.log(parsedData["garmin_fit"]["paramnameconversion"]);
 
-});
+function read_toml() {
 
-readStream.on('error', (err) => {
-    console.error(`Error reading TOML file: ${err}`);
-});
+    // Specify the path to your TOML file
+    const tomlFilePath = "config_dummy.toml";
+
+    // Create a read stream for the TOML file
+    const readStream = fs.createReadStream(tomlFilePath, 'utf8');
+
+    let data = '';
+
+    readStream.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    readStream.on('end', () => {
+        // Now, the `data` variable contains the entire contents of the TOML file as a string
+        console.log(data);
+
+        // You can parse it as TOML if needed
+        const toml = require('toml');
+
+        try {
+            parsedData = toml.parse(data);
+        } catch (e) {
+            console.error("Parsing error on line " + e.line + ", column " + e.column +
+                ": " + e.message);
+        }
+
+        // Now you can work with the parsed TOML data
+        console.log(parsedData["garmin_fit"]["paramnameconversion"]);
+
+    });
+
+    readStream.on('error', (err) => {
+        console.error(`Error reading TOML file: ${err}`);
+    });
+    return data
+}
