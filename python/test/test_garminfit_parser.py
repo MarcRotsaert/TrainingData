9ae07310f813520a7633c70a1f76fa9678c0f889
MarcRotsaert <mglrot@caiway.nl>
2023-11-15 17:37:56 +0100

    reshufle of garminfit-parser

--- python/test/test_garminfit_parser.py
+++ python/test/test_garminfit_parser.py
@@ -1,4 +1,3 @@
-
 import unittest
 import time
 import tomli
@@ -13,23 +12,22 @@ class GarminfitParser(unittest.TestCase):
 
     def test_lapparser_example1(self):
         file = "marcrotsaert_158946109.fit"
-        alaps = gar_par.Lapparser(file).fit2laps('alaps')
-        laps = gar_par.Lapparser(file).fit2laps('laps')
+        alaps = gar_par.Lapparser(file).fit2laps("alaps")
+        laps = gar_par.Lapparser(file).fit2laps("laps")
         with self.subTest():
             self.assertEqual(len(alaps), 2)
         with self.subTest():
             self.assertEqual(len(laps), 0)
 
-
     def test_lapparser_example2(self):
         file = "marcrotsaert_401780441.fit"
-        alaps = gar_par.Lapparser(file).fit2laps('alaps')
-        laps = gar_par.Lapparser(file).fit2laps('laps')
+        alaps = gar_par.Lapparser(file).fit2laps("alaps")
+        laps = gar_par.Lapparser(file).fit2laps("laps")
         with self.subTest():
             self.assertEqual(len(alaps), 18)
         with self.subTest():
-            self.assertEqual(alaps[0]['distance'], 5000)
-            self.assertEqual(alaps[-1]['distance'], 1857.0)
+            self.assertEqual(alaps[0]["distance"], 5000)
+            self.assertEqual(alaps[-1]["distance"], 1857.0)
         with self.subTest():
             self.assertEqual(len(laps), 1)
 
@@ -52,12 +50,12 @@ class GarminfitParser(unittest.TestCase):
         with self.subTest():
             self.assertIn("exercises", json)
         with self.subTest():
-            self.assertEqual(len(json["exercises"]),1)
+            self.assertEqual(len(json["exercises"]), 1)
         with self.subTest():
-            self.assertTrue('samples' in json["exercises"][0])
+            self.assertTrue("samples" in json["exercises"][0])
         with self.subTest():
-            self.assertTrue('recordedRoute' in json["exercises"][0]['samples'])
-     
+            self.assertTrue("recordedRoute" in json["exercises"][0]["samples"])
+
     def test_fit2json_example1(self):
         file = file = "marcrotsaert_158946109.fit"
         json = gar_par.Parser(file).fit2json()
@@ -72,11 +70,11 @@ class GarminfitParser(unittest.TestCase):
         with self.subTest():
             self.assertIn("exercises", json)
         with self.subTest():
-            self.assertEqual(len(json["exercises"]),1)
+            self.assertEqual(len(json["exercises"]), 1)
         with self.subTest():
-            self.assertTrue('samples' in json["exercises"][0])
+            self.assertTrue("samples" in json["exercises"][0])
         with self.subTest():
-            self.assertTrue('recordedRoute' in json["exercises"][0]['samples'])
+            self.assertTrue("recordedRoute" in json["exercises"][0]["samples"])
 
     def test_fit2json_example2(self):
         file = file = "marcrotsaert_401780441.fit"
@@ -90,8 +88,14 @@ class GarminfitParser(unittest.TestCase):
         with self.subTest():
             self.assertIn("exercises", json)
         with self.subTest():
-            self.assertEqual(len(json["exercises"]),1)
+            self.assertEqual(len(json["exercises"]), 1)
         with self.subTest():
-            self.assertTrue('samples' in json["exercises"][0])
+            self.assertTrue("samples" in json["exercises"][0])
         with self.subTest():
-            self.assertTrue('recordedRoute' in json["exercises"][0]['samples'])
+            self.assertTrue("recordedRoute" in json["exercises"][0]["samples"])
+
+    def test_abstract_parsing(self):
+        file = "marcrotsaert_158946109.fit"
+        abstract = gar_par.Parser(file).extract_abstract()
+        self.assertEqual(abstract["sport"], "CYCLING")
+        self.assertEqual(abstract["maximumHeartRate"], 150)
