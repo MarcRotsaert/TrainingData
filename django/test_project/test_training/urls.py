2b3fde982bf154dc804b9d1f6116ed49d53a1692
Marc Rotsaert <marcrotsaert@hotmail.com>
2023-11-29 15:41:42 +0100

    templating, filter

--- django/test_project/test_training/urls.py
+++ django/test_project/test_training/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from test_training import views
+
+app_name = "test_training"
 
 urlpatterns = [
     path('training/', views.select_ttype, name="testpage"),
     path('select_ttype/', views.select_ttype2, name="selectttype"),
-    path("add_ttype/",views.add_ttype, name="addtype")
+    # re_path(r"^add_ttype/$", views.add_ttype, name="addttype")
+    path("add_ttype/", views.add_ttype, name="addttype")
     ]
